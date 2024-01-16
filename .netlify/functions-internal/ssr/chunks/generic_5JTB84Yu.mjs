ca5a9ed7630e4f00cb717fbac47b9b3863450d62
Soy_NooV <heroyi3000@gmail.com>
2024-01-16 13:16:09 +0100

    Update

---
+++ .netlify/functions-internal/ssr/chunks/generic_5JTB84Yu.mjs
@@ -0,0 +1,6 @@
+export { renderers } from '../renderers.mjs';
+export { onRequest } from '../_empty-middleware.mjs';
+
+const page = () => import('./pages/generic_8JezHusL.mjs');
+
+export { page };
---
+++ .netlify/functions-internal/ssr/chunks/pages/index_8Une84-i.mjs
@@ -0,0 +1,25 @@
+/* empty css                          */
+import { e as createAstro, f as createComponent, r as renderTemplate, h as addAttribute, i as renderHead, j as renderSlot, k as renderComponent, m as maybeRenderHead } from '../astro_Tj6vjdcQ.mjs';
+import 'kleur/colors';
+import 'html-escaper';
+import 'clsx';
+
+const $$Astro$1 = createAstro();
+const $$Layout = createComponent(async ($$result, $$props, $$slots) => {
+  const Astro2 = $$result.createAstro($$Astro$1, $$props, $$slots);
+  Astro2.self = $$Layout;
+  const { title } = Astro2.props;
+  return renderTemplate`<html lang="es"> <head><meta charset="UTF-8"><meta name="description" content="Astro description"><meta name="viewport" content="width=device-width"><link rel="icon" type="image/svg+xml" href="/favicon.svg"><meta name="generator"${addAttribute(Astro2.generator, "content")}><title>${title}</title>${renderHead()}</head> <body> ${renderSlot($$result, $$slots["default"])} </body></html>`;
+}, "D:/PROJECTS/WEB/voidegon/src/layouts/Layout.astro", void 0);
+
+const $$Astro = createAstro();
+const $$Index = createComponent(async ($$result, $$props, $$slots) => {
+  const Astro2 = $$result.createAstro($$Astro, $$props, $$slots);
+  Astro2.self = $$Index;
+  return renderTemplate`${renderComponent($$result, "Layout", $$Layout, { "title": "| Voidegon" }, { "default": ($$result2) => renderTemplate` ${maybeRenderHead()}<main></main> ` })}`;
+}, "D:/PROJECTS/WEB/voidegon/src/pages/index.astro", void 0);
+
+const $$file = "D:/PROJECTS/WEB/voidegon/src/pages/index.astro";
+const $$url = "";
+
+export { $$Index as default, $$file as file, $$url as url };
