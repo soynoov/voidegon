6439e45a02d9800700b4abb88bd5a866aad1990f
Soy_NooV <heroyi3000@gmail.com>
2024-01-24 23:18:38 +0100

    Update

--- .netlify/functions-internal/ssr/chunks/characters_DRgM8E_-.mjs
+++
@@ -1,6 +0,0 @@
-export { renderers } from '../renderers.mjs';
-export { onRequest } from '../_empty-middleware.mjs';
-
-const page = () => import('./pages/characters_wczbX6Qm.mjs').then(n => n.e);
-
-export { page };
--- .netlify/functions-internal/ssr/chunks/extras_hpXGGEHA.mjs
+++
@@ -1,6 +0,0 @@
-export { renderers } from '../renderers.mjs';
-export { onRequest } from '../_empty-middleware.mjs';
-
-const page = () => import('./pages/extras_L64vAw6j.mjs');
-
-export { page };
